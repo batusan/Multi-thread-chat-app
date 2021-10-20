3e6292d082d70be41f93ab4238518e510865c5c4
Batuhan San <54415339+batusan@users.noreply.github.com>
2021-10-20 20:49:42 +0300

    Update User.java

--- src/com/gazi/batuhansan/models/User.java
+++ src/com/gazi/batuhansan/models/User.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.gazi.batuhansan.models;
 
 import java.io.ObjectOutputStream;
@@ -12,14 +7,6 @@ import java.io.ObjectOutputStream;
  * @author BatuPC
  */
 public class User {
-    /**
-     * Bu classın kullanılma amacı , sunucuya bağlanan her bir socketin takibini yapmaktır.
-     * her bağlanan socketin output streamini ve usernameini bir arada tutabilmek için bu model classı oluşturulmuştur.
-     * 
-     * Constructorunda output streamini ve usernameini alarak bağlanan her bir socketi etiketlemiş oluyoruz.
-     * 
-     * getOutputObject ve getUsername private fieldları return ediyorlar sonradan set edilme özelliklerine sahip değiller.
-     */
     private ObjectOutputStream objOutputStream;
     private String username;
     
